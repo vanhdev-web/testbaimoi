58ca7be5706375d6a294fb807586b17ddfeb8393
Vietanh <dovietanh240405@gmail.com>
2025-05-10 08:47:57 +0700

    new

---
+++ storeProcedure/storeProcedure/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace storeProcedure
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listViewDSPN = new System.Windows.Forms.ListView();
+            this.listViewTT = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(145, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tuy vấn dữ liệu";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listViewDSPN);
+            this.groupBox1.Location = new System.Drawing.Point(17, 78);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(282, 324);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Danh sách phiếu nhập";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listViewTT);
+            this.groupBox2.Location = new System.Drawing.Point(323, 78);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(282, 324);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Thông tin chi tiết phiếu nhập";
+            // 
+            // listViewDSPN
+            // 
+            this.listViewDSPN.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listViewDSPN.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewDSPN.FullRowSelect = true;
+            this.listViewDSPN.GridLines = true;
+            this.listViewDSPN.HideSelection = false;
+            this.listViewDSPN.Location = new System.Drawing.Point(3, 16);
+            this.listViewDSPN.Name = "listViewDSPN";
+            this.listViewDSPN.Size = new System.Drawing.Size(276, 305);
+            this.listViewDSPN.TabIndex = 0;
+            this.listViewDSPN.UseCompatibleStateImageBehavior = false;
+            this.listViewDSPN.View = System.Windows.Forms.View.Details;
+            this.listViewDSPN.SelectedIndexChanged += new System.EventHandler(this.listViewDSPN_SelectedIndexChanged);
+            // 
+            // listViewTT
+            // 
+            this.listViewTT.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7});
+            this.listViewTT.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewTT.FullRowSelect = true;
+            this.listViewTT.GridLines = true;
+            this.listViewTT.HideSelection = false;
+            this.listViewTT.Location = new System.Drawing.Point(3, 16);
+            this.listViewTT.Name = "listViewTT";
+            this.listViewTT.Size = new System.Drawing.Size(276, 305);
+            this.listViewTT.TabIndex = 0;
+            this.listViewTT.UseCompatibleStateImageBehavior = false;
+            this.listViewTT.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "SoPN";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "NgayNhap";
+            this.columnHeader2.Width = 80;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "SoDH";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "SoPN";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "MaVtu";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Số lượng";
+            this.columnHeader6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "DgNhap";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView listViewDSPN;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView listViewTT;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+    }
+}
+
---
+++ storeProcedure/storeProcedure/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace storeProcedure
+{
+    public partial class Form1 : Form
+    {
+        string strcon = "Data Source=LAPTOP-ECL79PA1\\MSSQLSERVER01;Initial Catalog=Quanlybanhang;Integrated Security=True;TrustServerCertificate=True";
+        SqlConnection sqlcon = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (sqlcon == null)
+            {
+                sqlcon = new SqlConnection(strcon);
+            }
+
+            if (sqlcon.State == ConnectionState.Closed)
+            {
+                sqlcon.Open();
+            }
+
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.CommandType  = CommandType.StoredProcedure;
+            sqlCommand.CommandText = "hienThiDanhSachPhieuNhap";
+
+            sqlCommand.Connection = sqlcon;
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+            while(reader.Read())
+            {
+                string soPN = reader.GetString(0);
+                DateTime ngay = reader.GetDateTime(1);
+                string soDH = reader.GetString(2);  
+
+                ListViewItem item = new ListViewItem(soPN);
+                item.SubItems.Add(ngay.ToString("dd/mm/yyyy"));
+                item.SubItems.Add(soDH);
+
+                listViewDSPN.Items.Add(item);
+            }
+            reader.Close();
+        }
+
+        private void listViewDSPN_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listViewDSPN.SelectedItems.Count > 0)
+            {
+                string soPN = listViewDSPN.SelectedItems[0].SubItems[0].Text;
+                hienThiChiTietPhieuNhap(soPN);
+            }
+        }
+
+        private void hienThiChiTietPhieuNhap(string soPN)
+        {
+            if (sqlcon == null)
+            {
+                sqlcon = new SqlConnection(strcon);
+            }
+
+            if (sqlcon.State == ConnectionState.Closed)
+            {
+                sqlcon.Open();
+            }
+
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+            sqlCommand.CommandText = "hienThiChiTietPhieuNhap";
+
+            SqlParameter para = new SqlParameter("@MaPN", SqlDbType.Char);
+            para.Value = soPN;
+            sqlCommand.Parameters.Add(para);
+
+            sqlCommand.Connection = sqlcon;
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+            listViewTT.Items.Clear();   
+            while (reader.Read())
+            {
+                string maVtu = reader.GetString(1);
+                string sl = reader.GetInt32(2).ToString();
+                string dg = reader.GetDecimal(3).ToString();
+                ListViewItem  item = new ListViewItem(soPN);
+                item.SubItems.Add(maVtu);
+                item.SubItems.Add(sl);
+                item.SubItems.Add(dg);
+
+                listViewTT.Items.Add(item);
+                
+            }
+            reader.Close();
+        }
+    }
+}
